4bc8ab93f4ccca7e72e3a93d17f7c5ec1c0c4469
joshbenaron <73971531+joshbenaron@users.noreply.github.com>
2021-07-04 17:38:17 +0100

    v0.1.1

--- src/ar-data-verify.d.ts
+++
@@ -1,4 +0,0 @@
-import Bundle from "./Bundle";
-import DataItem from "./DataItem";
-export declare function verifyBundle(bundle: Bundle): Promise<boolean>;
-export declare function verifyData(item: DataItem): Promise<boolean>;
--- src/utils.d.ts
+++
@@ -1,5 +0,0 @@
-export declare function longTo8ByteArray(long: number): number[];
-export declare function longTo16ByteArray(long: number): number[];
-export declare function longTo32ByteArray(long: number): number[];
-export declare function byteArrayToLong(byteArray: Uint8Array): number;
-export declare function arraybufferEqual(buf1: Uint8Array, buf2: Uint8Array): boolean;
