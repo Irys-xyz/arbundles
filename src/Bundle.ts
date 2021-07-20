e93bfe9f24beb13e4b762c5a4bb901040766135c
joshbenaron <73971531+joshbenaron@users.noreply.github.com>
2021-07-20 15:43:18 +0100

    Add base64url encoding to getId functions

--- src/Bundle.ts
+++ src/Bundle.ts
@@ -44,7 +44,7 @@ export default class Bundle {
   public getIds(): string[] {
     const ids = [];
     for (let i = HEADER_START; i < (HEADER_START + (64 * this.length)); i+=64) {
-      ids.push(base64url.encode(this.binary.slice(i + 32, i + 64)))
+      ids.push(base64url.encode(this.binary.slice(i + 32, i + 64)), "hex")
     }
 
     return ids;
@@ -56,7 +56,7 @@ export default class Bundle {
     }
 
     const start = 64 + (64 * index);
-    return base64url.encode(this.binary.slice(start, start + 32));
+    return base64url.encode(this.binary.slice(start, start + 32), "hex");
   }
 
   public getAll(): DataItem[] {
