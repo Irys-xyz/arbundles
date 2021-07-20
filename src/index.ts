9867e65a639b74510a2052327c2081fceed0e8a8
joshbenaron <73971531+joshbenaron@users.noreply.github.com>
2021-07-20 21:11:37 +0100

    Build and push new version

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,14 @@ import { createData } from "./ar-data-create";
 import { bundleAndSignData, unbundleData } from "./ar-data-bundle";
 import { verifyFile } from './ar-data-verify';
 import Bundle from "./Bundle";
-import DataItem from "./DataItem";
+import DataItem, { MIN_BINARY_SIZE } from './DataItem';
 import { tagsParser } from "./parser";
 import deepHash from './deepHash';
 
 const checkTags = tagsParser.isValid;
 
 export {
+  MIN_BINARY_SIZE,
   Bundle,
   DataItem,
   createData,
