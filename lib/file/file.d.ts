c8accead30661e6ef03227124ab5d2a8f5e4e5a5
Josh <joshbenaron@gmail.com>
2021-07-30 21:40:26 +0100

    Build and push new version

--- lib/file/file.d.ts
+++ lib/file/file.d.ts
@@ -2,3 +2,17 @@ export declare function getTags(filename: string): Promise<{
     name: string;
     value: string;
 }[]>;
+interface Transaction {
+    id: string;
+    owner: string;
+    tags: {
+        name: string;
+        value: string;
+    }[];
+    target: string;
+    data_size: number;
+    fee: number;
+    signature: string;
+}
+export declare function fileToJson(filename: string): Promise<Transaction>;
+export {};
