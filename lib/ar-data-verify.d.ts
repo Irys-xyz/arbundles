31e39ddab91a670e8a414a7f077a83f9e78c63bc
Josh <joshbenaron@gmail.com>
2021-08-03 23:10:25 +0100

    Build and push new version

--- lib/ar-data-verify.d.ts
+++ lib/ar-data-verify.d.ts
@@ -1,8 +1,11 @@
-import Bundle from "./Bundle";
+import Bundle from './Bundle';
 /**
  * Verifies a bundle and all of its DataItems
  *
  * @param bundle
  */
 export declare function verifyBundle(bundle: Bundle): boolean;
-export declare function verifyFile(filename: string): Promise<boolean>;
+export declare function verifyDataItemInFile(filename: string, signatureVerification?: {
+    n: string;
+    signature: Uint8Array;
+}): Promise<boolean>;
--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,11 +1,11 @@
 import { createData } from "./ar-data-create";
 import { bundleAndSignData, unbundleData } from "./ar-data-bundle";
-import { verifyFile } from './ar-data-verify';
+import { verifyDataItemInFile } from './ar-data-verify';
 import Bundle from "./Bundle";
 import DataItem, { MIN_BINARY_SIZE } from './DataItem';
 import deepHash from './deepHash';
 import { DataItemCreateOptions } from './ar-data-base';
 import { fileToJson, getTags } from './file/file';
 declare const checkTags: (val: any, opts?: Partial<import("avsc/types").IsValidOptions>) => boolean;
-export { MIN_BINARY_SIZE, Bundle, DataItem, createData, bundleAndSignData, unbundleData, verifyFile, deepHash, checkTags, DataItemCreateOptions };
+export { MIN_BINARY_SIZE, Bundle, DataItem, createData, bundleAndSignData, unbundleData, verifyDataItemInFile, deepHash, checkTags, DataItemCreateOptions };
 export { getTags, fileToJson };
