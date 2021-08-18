f3695d8dfa04cbdca8e85119724b6cabd52129fb
Josh <joshbenaron@gmail.com>
2021-08-18 15:07:31 +0100

    Build and push new version

--- lib/file/file.d.ts
+++ lib/file/file.d.ts
@@ -1,33 +1,26 @@
-declare module "ans104/file" {
-  type File = string | number;
-  export function getTags(filename: string): Promise<{
+declare type File = string | number;
+export declare function getTags(filename: string): Promise<{
     name: string;
     value: string;
-  }[]>;
-  interface Transaction {
+}[]>;
+interface Transaction {
     id: string;
     owner: string;
     tags: {
-      name: string;
-      value: string;
+        name: string;
+        value: string;
     }[];
     target: string;
     data_size: number;
     fee: number;
     signature: string;
-  }
-  export function fileToJson(filename: string): Promise<Transaction>;
-  export function numberOfItems(file: File): Promise<number>;
-  interface DataItemHeader {
+}
+export declare function fileToJson(filename: string): Promise<Transaction>;
+export declare function numberOfItems(file: File): Promise<number>;
+interface DataItemHeader {
     offset: number;
     id: string;
-  }
-  export function getHeaderAt(file: File | number, index: number): Promise<DataItemHeader>;
-  export function getHeaders(file: File): AsyncGenerator<DataItemHeader>;
-  export {};
-
 }
-
-declare module 'ans104/file' {
-  export * from 'ans104/file';
-}
+export declare function getHeaderAt(file: File | number, index: number): Promise<DataItemHeader>;
+export declare function getHeaders(file: File): AsyncGenerator<DataItemHeader>;
+export {};
