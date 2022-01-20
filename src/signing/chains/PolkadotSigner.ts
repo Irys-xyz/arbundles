d8ed975928e72959a81a61d9861dcaf7179fe9da
JesseTheRobot <jesse.cruz.wright@gmail.com>
2022-01-20 22:18:59 +0000

    add ready method for async WASM

--- src/signing/chains/PolkadotSigner.ts
+++ src/signing/chains/PolkadotSigner.ts
@@ -1,8 +1,10 @@
+import { cryptoWaitReady } from "@polkadot/util-crypto";
 import Sr25519 from "../keys/sr25519";
 export default class PolkadotSigner extends Sr25519 {
-
-    constructor(privateKey:string){
-        super(privateKey)
-    }
-
+  constructor(privateKey: string) {
+    super(privateKey);
+  }
+  public async ready(): Promise<void> {
+    await cryptoWaitReady();
+  }
 }
