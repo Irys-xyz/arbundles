9bdb136ca76f0fa2ccd887d40d7c8c6de0936a98
rabm2k7 <rabmiller1@gmail.com>
2022-02-02 16:58:53 +0000

    Co-authored-by: Jesse Cruz Wright <jesse.cruz.wright@gmail.com>

--- src/signing/keys/sr25519.ts
+++ src/signing/keys/sr25519.ts
@@ -1,7 +1,6 @@
 import { Signer } from "../Signer";
 import { Keyring } from "@polkadot/keyring";
 import { cryptoWaitReady, signatureVerify } from "@polkadot/util-crypto";
-import { Keypair } from "@polkadot/util-crypto/types";
 // import { SubmittableExtrinsic } from "@polkadot/api/types";
 import { SIG_CONFIG } from "../../constants";
 // import base58 from "bs58";
@@ -11,11 +10,12 @@ export default class Sr25519 implements Signer {
   readonly ownerLength: number = SIG_CONFIG[4].pubLength;
   readonly signatureLength: number = SIG_CONFIG[4].sigLength;
   pem?: string | Buffer;
-  protected keyPair: Keypair;
+  protected keyPair: any; // because Polkadot is awesome
   protected pk: Buffer;
   protected _pk;
   protected keyring;
-  signature;
+  public polkaPair
+  
   constructor(privateKey: string) {
     this._pk = privateKey;
   }
@@ -25,15 +25,14 @@ export default class Sr25519 implements Signer {
   }
 
   sign(message: Uint8Array): Uint8Array | Promise<Uint8Array>{
-      const signer = this.signature
-      return signer.sign(message);
+      return this.keyPair.sign(message);
   }
 
   public async ready(): Promise<void> {
     await cryptoWaitReady();
     this.keyring = new Keyring({ type: "sr25519", ss58Format: 0 });
-    this.signature = this.keyring.createFromUri(this._pk, { name: "sr25519" });
-    this.keyPair = this.signature;
+    this.keyPair = this.keyring.createFromUri(this._pk, { name: "sr25519" });
+    this.polkaPair = this.keyPair;
     this.pk = Buffer.from(this.keyPair.publicKey);
   }
 
