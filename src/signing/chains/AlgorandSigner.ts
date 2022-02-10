f30781ab4c2a79f5358fe47f699447e861c7245f
rabm2k7 <rabmiller1@gmail.com>
2022-02-10 16:41:01 +0000

    update signer

--- src/signing/chains/AlgorandSigner.ts
+++ src/signing/chains/AlgorandSigner.ts
@@ -1,7 +1,15 @@
-import SolanaSigner from "./SolanaSigner";
+import Curve25519 from "../keys/curve25519";
+export default class AlgorandSigner extends Curve25519 {
+  
+  get publicKey(): Buffer {
+    return Buffer.from(this.pk);
+  }
 
-export default class AlgorandSigner extends SolanaSigner {
-  constructor(_key: string) {
-    super(_key.replace("ed25519:", ""));
+  get key(): Uint8Array {
+    return Buffer.from(this._key);
+  }
+
+  constructor(_key: any, pk: any) {
+    super(_key.subarray(0, 32), pk);
   }
 }
