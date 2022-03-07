4cce638d552e4e053e3a7fb50001a966b3909568
rabm2k7 <rabmiller1@gmail.com>
2022-03-07 16:36:24 +0000

    remove comments

--- src/signing/chains/CosmosSigner.ts
+++ src/signing/chains/CosmosSigner.ts
@@ -1,9 +1,6 @@
 import { Signer } from "../Signer";
 import { SignatureConfig, SIG_CONFIG } from "../../constants";
 import { Secp256k1, sha256, Bip39, Slip10, stringToPath, Slip10Curve, EnglishMnemonic, Secp256k1Signature } from "@cosmjs/crypto";
-// import * as aminocosmos from "@cosmjs/amino";
-// import * as signingcosmos from "@cosmjs/proto-signing";
-// import * as encode from "@cosmjs/encoding";
 
 export default class CosmosSigner implements Signer {
   readonly ownerLength: number = SIG_CONFIG[SignatureConfig.COSMOS].pubLength;
@@ -12,8 +9,6 @@ export default class CosmosSigner implements Signer {
   pem?: string | Buffer;
 
   protected wallet;
-  // protected keyring;
-  // public keyPair;
   public pk;
   public signingKp;
   
@@ -26,11 +21,6 @@ export default class CosmosSigner implements Signer {
   }
 
   public async ready(): Promise<void> {
-    // this.keyring = await aminocosmos.Secp256k1HdWallet.fromMnemonic(this.wallet);
-    // const [first] = await this.keyring.getAccounts();
-    // this.keyPair = first;
-    // this.pk = this.keyPair.pubkey;
-
     const walletSeed = await Bip39.mnemonicToSeed(new EnglishMnemonic(this.wallet));
     const path = stringToPath("m/44'/118'/0'/0/0");
     const slip = Slip10.derivePath(Slip10Curve.Secp256k1, walletSeed ,path);
