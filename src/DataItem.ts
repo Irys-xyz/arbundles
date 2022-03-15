c46a390e05a5c667aea3d05cd133cffae885456b
JesseTheRobot <jesse.cruz.wright@gmail.com>
2022-03-15 14:44:53 +0000

    Add phantom as a new sig type

--- src/DataItem.ts
+++ src/DataItem.ts
@@ -39,6 +39,9 @@ export default class DataItem implements BundleItem {
       case 3: {
         return SignatureConfig.ETHEREUM;
       }
+      case 4: {
+        return SignatureConfig.PHANTOM;
+      }
       default: {
         throw new Error("Unknown signature type: " + signatureTypeVal);
       }
---
+++ src/signing/chains/phantomSigner.ts
@@ -0,0 +1,29 @@
+import base64url from "base64url";
+import InjectedSolanaSigner from "./injectedSolanaSigner";
+import * as ed25519 from "noble-ed25519";
+
+export default class PhantomSigner extends InjectedSolanaSigner {
+  signatureType = 3; // for Phantom signer type
+
+  constructor(provider) {
+    super(provider);
+  }
+
+  async sign(message: Uint8Array): Promise<Uint8Array> {
+    return super.sign(Buffer.from(Buffer.from(message).toString("hex")));
+  }
+
+  static async verify(
+    pk: Buffer,
+    message: Uint8Array,
+    signature: Uint8Array,
+  ): Promise<boolean> {
+    let p = pk;
+    if (typeof pk === "string") p = base64url.toBuffer(pk);
+    return ed25519.verify(
+      Buffer.from(signature),
+      Buffer.from(message).toString("hex"),
+      Buffer.from(p),
+    );
+  }
+}
