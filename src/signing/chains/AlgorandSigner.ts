8015e97064de0b686d513608d9b81a885ee10d4b
rabm2k7 <rabmiller1@gmail.com>
2022-05-19 11:05:15 +0100

    CHORE: Return signers to original

--- src/signing/chains/AlgorandSigner.ts
+++ src/signing/chains/AlgorandSigner.ts
@@ -1,14 +1,14 @@
-import Curve25519 from "../keys/curve25519";
-export default class AlgorandSigner extends Curve25519 {
-  get publicKey(): Buffer {
-    return Buffer.from(this.pk);
-  }
+// import Curve25519 from "../keys/curve25519";
+// export default class AlgorandSigner extends Curve25519 {
+//   get publicKey(): Buffer {
+//     return Buffer.from(this.pk);
+//   }
 
-  get key(): Uint8Array {
-    return Buffer.from(this._key);
-  }
+//   get key(): Uint8Array {
+//     return Buffer.from(this._key);
+//   }
 
-  constructor(key: any, pk: any) {
-    super(key.subarray(0, 32), pk);
-  }
-}
+//   constructor(key: any, pk: any) {
+//     super(key.subarray(0, 32), pk);
+//   }
+// }
--- src/signing/chains/SolanaSigner.ts
+++ src/signing/chains/SolanaSigner.ts
@@ -1,17 +1,17 @@
-import Curve25519 from "../keys/curve25519";
-import bs58 from "bs58";
+// import Curve25519 from "../keys/curve25519";
+// import bs58 from "bs58";
 
-export default class SolanaSigner extends Curve25519 {
-  get publicKey(): Buffer {
-    return bs58.decode(this.pk);
-  }
+// export default class SolanaSigner extends Curve25519 {
+//   get publicKey(): Buffer {
+//     return bs58.decode(this.pk);
+//   }
 
-  get key(): Uint8Array {
-    return bs58.decode(this._key);
-  }
+//   get key(): Uint8Array {
+//     return bs58.decode(this._key);
+//   }
 
-  constructor(_key: string) {
-    const b = bs58.decode(_key);
-    super(bs58.encode(b.subarray(0, 32)), bs58.encode(b.subarray(32, 64)));
-  }
-}
+//   constructor(_key: string) {
+//     const b = bs58.decode(_key);
+//     super(bs58.encode(b.subarray(0, 32)), bs58.encode(b.subarray(32, 64)));
+//   }
+// }
