8d0e1e9d5aa32e238163a3de9f32471942a5e0c7
JesseTheRobot <jesse.cruz.wright@gmail.com>
2022-11-25 23:17:26 +0000

    feat: :construction: v1 Enhanced ethereum signer
    Uses native crypto bindings to massively improve performance.

--- src/signing/chains/ethereumSigner.ts
+++ src/signing/chains/ethereumSigner.ts
@@ -1,6 +1,6 @@
 import Secp256k1 from "../keys/secp256k1";
 import secp256k1 from "secp256k1";
-import { ethers } from "ethers";
+import { arrayify, hashMessage } from "ethers/lib/utils";
 
 export default class EthereumSigner extends Secp256k1 {
   get publicKey(): Buffer {
@@ -14,10 +14,15 @@ export default class EthereumSigner extends Secp256k1 {
   }
 
   sign(message: Uint8Array): Uint8Array {
-    const wallet = new ethers.Wallet(this._key);
-    return wallet
-      .signMessage(message)
-      .then((r) => Buffer.from(r.slice(2), "hex")) as any;
+    // const wallet = new ethers.Wallet(this._key);
+    // return wallet
+    //   .signMessage(message)
+    //   .then((r) => Buffer.from(r.slice(2), "hex")) as any;
+    return Buffer.concat([
+      Buffer.from(
+        secp256k1.ecdsaSign(arrayify(hashMessage(message)), this.key).signature,
+      ),
+    ]);
   }
 
   static async verify(
@@ -25,7 +30,12 @@ export default class EthereumSigner extends Secp256k1 {
     message: Uint8Array,
     signature: Uint8Array,
   ): Promise<boolean> {
-    const address = ethers.utils.computeAddress(pk);
-    return ethers.utils.verifyMessage(message, signature) === address;
+    // const address = ethers.utils.computeAddress(pk);
+    // return ethers.utils.verifyMessage(message, signature) === address;
+    return super.verify(
+      pk,
+      arrayify(hashMessage(message)),
+      signature.slice(0, -1),
+    );
   }
 }
