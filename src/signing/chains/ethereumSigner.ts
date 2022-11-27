8f4c26ea2e91cf0fb5d665fa4ac1cb2e9221fd1e
Jesse Cruz Wright <jesse.cruz.wright@gmail.com>
2022-11-27 12:47:04 +0000

    feat: Enhanced Ethereum signer (#60)
    * feat: :construction: v1 Enhanced ethereum signer
    
    Uses native crypto bindings to massively improve performance.
    
    * fix: Add missing verification component from signature
    
    * fix: Ethereum signer verification and signing

--- src/signing/chains/ethereumSigner.ts
+++ src/signing/chains/ethereumSigner.ts
@@ -1,5 +1,7 @@
 import Secp256k1 from "../keys/secp256k1";
 import secp256k1 from "secp256k1";
+import { arrayify, hashMessage } from "ethers/lib/utils";
+import base64url from "base64url";
 import { ethers } from "ethers";
 
 export default class EthereumSigner extends Secp256k1 {
@@ -18,6 +20,8 @@ export default class EthereumSigner extends Secp256k1 {
     return wallet
       .signMessage(message)
       .then((r) => Buffer.from(r.slice(2), "hex")) as any;
+    // below doesn't work due to lacking correct v derivation.
+    //return Buffer.from(joinSignature(Buffer.from(secp256k1.ecdsaSign(arrayify(hashMessage(message)), this.key).signature)).slice(2), "hex");
   }
 
   static async verify(
@@ -25,7 +29,12 @@ export default class EthereumSigner extends Secp256k1 {
     message: Uint8Array,
     signature: Uint8Array,
   ): Promise<boolean> {
-    const address = ethers.utils.computeAddress(pk);
-    return ethers.utils.verifyMessage(message, signature) === address;
+    // const address = ethers.utils.computeAddress(pk);
+    // return ethers.utils.verifyMessage(message, signature) === address;
+    return secp256k1.ecdsaVerify(
+      (signature.length === 65) ? signature.slice(0, -1) : signature,
+      arrayify(hashMessage(message)),
+      (typeof pk === "string") ? base64url.toBuffer(pk) : pk,
+    );
   }
 }
