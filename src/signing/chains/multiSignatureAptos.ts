b447edb501c82f9155642bb023dc16c8bc5546cf
JesseTheRobot <jesse.cruz.wright@gmail.com>
2023-01-10 00:15:30 +0000

    feat: :sparkles: Inline createBitmap to cut aptos dependency

--- src/signing/chains/multiSignatureAptos.ts
+++ src/signing/chains/multiSignatureAptos.ts
@@ -1,5 +1,4 @@
 import { Signer } from "..";
-import { TxnBuilderTypes } from "aptos";
 import * as ed25519 from "@noble/ed25519";
 // import nacl from 'tweetnacl';
 import { SignatureConfig, SIG_CONFIG } from "../../constants";
@@ -14,7 +13,7 @@ export default class MultiSignatureAptosSigner implements Signer {
 
   protected collectSignatures: (
     message: Uint8Array,
-  ) => Promise<{ signatures: Buffer[]; bitmap: number[] }>;
+  ) => Promise<{ signatures: Buffer[]; bitmap: number[]; }>;
 
   protected provider: any;
 
@@ -22,7 +21,7 @@ export default class MultiSignatureAptosSigner implements Signer {
     publicKey: Buffer,
     collectSignatures: (
       message: Uint8Array,
-    ) => Promise<{ signatures: Buffer[]; bitmap: number[] }>,
+    ) => Promise<{ signatures: Buffer[]; bitmap: number[]; }>,
   ) {
     this._publicKey = publicKey;
     this.collectSignatures = collectSignatures;
@@ -33,9 +32,36 @@ export default class MultiSignatureAptosSigner implements Signer {
   }
 
   async sign(message: Uint8Array): Promise<Uint8Array> {
-    const { signatures, bitmap } = await this.collectSignatures(message);
-    const generatedBitmap =
-      TxnBuilderTypes.MultiEd25519Signature.createBitmap(bitmap);
+    const { signatures, bitmap: bits } = await this.collectSignatures(message);
+    // Bits are read from left to right. e.g. 0b10000000 represents the first bit is set in one byte.
+    // The decimal value of 0b10000000 is 128.
+    const firstBitInByte = 128;
+    const bitmap = new Uint8Array([0, 0, 0, 0]);
+
+    // Check if duplicates exist in bits
+    const dupCheckSet = new Set();
+
+    bits.forEach((bit: number) => {
+      if (bit >= 32) {
+        throw new Error(`Invalid bit value ${bit}.`);
+      }
+
+      if (dupCheckSet.has(bit)) {
+        throw new Error("Duplicated bits detected.");
+      }
+
+      dupCheckSet.add(bit);
+
+      const byteOffset = Math.floor(bit / 8);
+
+      let byte = bitmap[byteOffset];
+
+      byte |= firstBitInByte >> bit % 8;
+
+      bitmap[byteOffset] = byte;
+    });
+
+
     const signature = Buffer.alloc(this.signatureLength);
     let sigPos = 0;
     for (let i = 0; i < 32; i++) {
@@ -43,10 +69,8 @@ export default class MultiSignatureAptosSigner implements Signer {
         signature.set(signatures[sigPos++], i * 64);
       }
     }
-    // signatures.forEach((s, i) => {
-    //     signature.set(s, i * 64)
-    // })
-    signature.set(generatedBitmap, this.signatureLength - 4);
+
+    signature.set(bitmap, this.signatureLength - 4);
     return signature;
   }
 
