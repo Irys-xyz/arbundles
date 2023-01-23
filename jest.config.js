99b982db422e82182f8472823a8bd0e08139bf41
JesseTheRobot <jesse.cruz.wright@gmail.com>
2023-01-23 01:21:37 +0000

    fix: :bug: Fix Jest with new paths

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,5 @@
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig.json');
 module.exports = {
   testEnvironment: "node",
   transform: {
@@ -8,4 +10,8 @@ module.exports = {
   coverageDirectory: "coverage",
   collectCoverageFrom: ["src/**/*.{ts,js}", "!src/**/*.d.ts"],
   setupFiles: ["<rootDir>/src/__tests__/setup.jest.js"],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
+  modulePaths: [
+    '<rootDir>'
+  ],
 };
