03dce5189762829f73477d6334384e15ea96f7d4
JesseTheRobot <jesse.cruz.wright@gmail.com>
2023-04-15 18:36:37 +0000

    fix: :sparkles: get crypto driver default hack working
    adds a dynamic .default property access for cryptoDriver

--- src/nodeUtils.ts
+++ src/nodeUtils.ts
@@ -1,18 +1,18 @@
-import { /* createHash, */ createPublicKey } from "crypto";
-// eslint-disable-next-line @typescript-eslint/consistent-type-imports
-import nodeDriver from "arweave/node/lib/crypto/node-driver.js";
-// import type { Hash } from "crypto";
+import { createPublicKey } from "crypto";
+import { default as nodeDriver } from "arweave/node/lib/crypto/node-driver.js";
 import type { JWKInterface } from "./interface-jwk.js";
-export { stringToBuffer, concatBuffers } from "arweave/node/lib/utils.js";
+// import CryptoInterface from "arweave/node/lib/crypto/crypto-interface.js";
+export { stringToBuffer, concatBuffers } from "arweave/node/lib/utils";
 export { default as Transaction } from "arweave/node/lib/transaction.js";
 export { deepHash } from "./deepHash.js";
+// import type { Hash } from "crypto";
 // export { default as Arweave } from "arweave/node";
 // export const sha384 = (): Hash => createHash("sha384");
 export type { CreateTransactionInterface } from "arweave/node/common.js";
 
-// @ts-expect-error hack to use .default in CJS environments
-const driver: typeof nodeDriver = typeof __importDefault === "undefined" ? nodeDriver.default : nodeDriver;
-
+// hack as ESM won't unpack .default CJS imports, so we do so dynamically
+// eslint-disable-next-line @typescript-eslint/dot-notation
+const driver: typeof nodeDriver = nodeDriver["default"] ? nodeDriver["default"] : nodeDriver;
 export class CryptoDriver extends driver {
   public getPublicKey(jwk: JWKInterface): string {
     return createPublicKey({
@@ -28,7 +28,7 @@ export class CryptoDriver extends driver {
   }
 }
 
-let d: CryptoDriver;
+let driverInstance: CryptoDriver;
 export function getCryptoDriver(): CryptoDriver {
-  return (d ??= new CryptoDriver());
+  return (driverInstance ??= new CryptoDriver());
 }
--- src/webUtils.ts
+++ src/webUtils.ts
@@ -1,27 +1,24 @@
-// import type { Hash } from "crypto";
 import type { JWKInterface } from "./interface-jwk.js";
 export type { default as Transaction } from "arweave/web/lib/transaction.js";
 export type { CreateTransactionInterface } from "arweave/web/common.js";
-// import { sha384 as SHA384 } from "sha.js";
 import webDriver from "arweave/web/lib/crypto/webcrypto-driver.js";
 export { stringToBuffer, concatBuffers } from "arweave/web/lib/utils.js";
 export { deepHash } from "./deepHash.js";
+// import { sha384 as SHA384 } from "sha.js";
 // export { default as Arweave } from "arweave/web";
-
+// import type { Hash } from "crypto";
 // export const sha384 = (): Hash => SHA384("sha384");
 
-// @ts-expect-error hack to use .default in CJS environments
-const driver: typeof webDriver = typeof __importDefault === "undefined" ? webDriver.default : webDriver;
-
+// hack as ESM won't unpack .default CJS imports, so we do so dynamically
+// eslint-disable-next-line @typescript-eslint/dot-notation
+const driver: typeof webDriver = webDriver["default"] ? webDriver["default"] : webDriver;
 export class CryptoDriver extends driver {
-  protected declare hasSubtle: boolean;
-
   public getPublicKey(_jwk: JWKInterface): string {
     throw new Error("Unimplemented");
   }
 }
 
-let d: CryptoDriver;
+let driverInstance: CryptoDriver;
 export function getCryptoDriver(): CryptoDriver {
-  return (d ??= new CryptoDriver());
+  return (driverInstance ??= new CryptoDriver());
 }
